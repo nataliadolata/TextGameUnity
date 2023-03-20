0bedf9255844bd4d578a9c3100d4ecc8a2d0887c
Natalia Dolata <93873092+nataliadolata@users.noreply.github.com>
2023-03-20 15:18:04 +0100

    Added new textgame script

---
+++ TextGameUnity/Assets/Scripts/TextGame.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextGame : MonoBehaviour
+{
+    // Program zgaduje liczbe ktora wymyslilismy sobie z konkretnego przedzialu liczbowego
+    // Mamy przedzial od 1 do 1000 i program pyta czy wymyslona liczba jest wieksza lub mniejsza lub rowna np. 328
+    // Uzytkownik odpowiada wieksza lub mniejsza lub rowna
+    int min = 1;
+    int max = 1000;
+    int shoot = 500;
+
+    void Start()
+    {
+        
+        Debug.Log("Witam w grze - pomysl w glowie liczbe od 1 do 1000");
+        Debug.Log("Czy twoja liczba jest rowna/mniejsza/wieksza od 500?");
+        // print string
+       
+    }
+
+   
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = shoot;
+            shoot = (min + max) / 2;
+            Debug.Log("Czy twoja liczba jest rowna/mniejsza/wieksza od "+shoot + " ?");
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = shoot;
+            shoot = (min + max) / 2;
+            Debug.Log("Czy twoja liczba jest rowna/mniejsza/wieksza od " + shoot + " ?");
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Komputer zgadl liczbe");
+        }
+    }
+}
