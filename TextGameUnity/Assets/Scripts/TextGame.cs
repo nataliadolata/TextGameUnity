4aa2ee95180c6d0125c9e0e685ed9187cbde1779
Natalia Dolata <93873092+nataliadolata@users.noreply.github.com>
2023-04-23 15:19:34 +0200

    changed script - bugfix 1001 - refactorization

--- TextGameUnity/Assets/Scripts/TextGame.cs
+++ TextGameUnity/Assets/Scripts/TextGame.cs
@@ -8,7 +8,7 @@ public class TextGame : MonoBehaviour
     // Mamy przedzial od 1 do 1000 i program pyta czy wymyslona liczba jest wieksza lub mniejsza lub rowna np. 328
     // Uzytkownik odpowiada wieksza lub mniejsza lub rowna
     int min = 1;
-    int max = 1000;
+    int max = 1001;
     int shoot = 500;
 
     void Start()
@@ -26,18 +26,22 @@ public class TextGame : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             min = shoot;
-            shoot = (min + max) / 2;
-            Debug.Log("Czy twoja liczba jest rowna/mniejsza/wieksza od "+shoot + " ?");
+            NextShoot();
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             max = shoot;
-            shoot = (min + max) / 2;
-            Debug.Log("Czy twoja liczba jest rowna/mniejsza/wieksza od " + shoot + " ?");
+            NextShoot();
         }
         if (Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("Komputer zgadl liczbe");
         }
     }
+    void NextShoot()
+    {
+        shoot = (min + max) / 2;
+        Debug.Log("Czy twoja liczba jest rowna/mniejsza/wieksza od " + shoot + " ?");
+    }
+            
 }
